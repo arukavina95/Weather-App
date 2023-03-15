0f5d3979ed921897d064648dea6b5c61ccbd88e1
Antun <antunrukavina8@gmail.com>
2023-03-15 12:42:00 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios'
 
 function App() {
+  const [data,setData] = useState({});
+  const [location,setLocation] = useState('');
+
+ const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5d5a78eb172372be098f139deadd143c&units=metric`;
+
+ const search =(event)=>{
+  if(event.key === 'Enter'){
+  axios.get(url).then((response)=>{
+    setData(response.data)
+    console.log(response.data)
+  })
+
+  setLocation('')
+ }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          placeholder="Enter Location"
+          onKeyPress={search}
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp}°C</h1> : null}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
